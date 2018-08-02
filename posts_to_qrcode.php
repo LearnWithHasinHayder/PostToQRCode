088fb186e3689eb4ef0706507d6bf6c93697efa5
Hasin Hayder <countdraculla@gmail.com>
2018-08-02 13:02:22 +0600

    Post To QR Code

---
+++ posts_to_qrcode.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Plugin Name: Posts To QR Code
+Plugin URI: https://learnwith.hasinhayder.com
+Description: Display QR Code under ever posts
+Version: 1.0
+Author: LWHH
+Author URI: https://hasin.me
+License: GPLv2 or later
+Text Domain: posts-to-qrcode
+Domain Path: /languages/
+*/
+
+/*function wordcount_activation_hook(){}
+register_activation_hook(__FILE__,"wordcount_activation_hook");
+
+function wordcount_deactivation_hook(){}
+register_deactivation_hook(__FILE__,"wordcount_deactivation_hook");*/
+
+function wordcount_load_textdomain() {
+    load_plugin_textdomain( 'posts-to-qrcode', false, dirname( __FILE__ ) . "/languages" );
+}
+
+function pqrc_display_qr_code( $content ) {
+    $current_post_id    = get_the_ID();
+    $current_post_title = get_the_title( $current_post_id );
+    $current_post_url   = urlencode( get_the_permalink( $current_post_id ) );
+    $current_post_type  = get_post_type( $current_post_id );
+
+    // Post Type Check
+
+    $excluded_post_types = apply_filters( 'pqrc_excluded_post_types', array() );
+    if ( in_array( $current_post_type, $excluded_post_types ) ) {
+        return $content;
+    }
+
+    //Dimension Hook
+    $dimension = apply_filters( 'pqrc_qrcode_dimension', '185x185' );
+
+    //Image Attributes
+    $image_attributes = apply_filters('pqrc_image_attributes',null);
+
+    $image_src = sprintf( 'https://api.qrserver.com/v1/create-qr-code/?size=%s&ecc=L&qzone=1&data=%s', $dimension, $current_post_url );
+    $content   .= sprintf( "<div class='qrcode'><img %s  src='%s' alt='%s' /></div>",$image_attributes, $image_src, $current_post_title );
+
+    return $content;
+}
+
+add_filter( 'the_content', 'pqrc_display_qr_code' );
+
+add_action('admin_init','admin');
+function admin(){
+    /*add_settings_section(
+        'my_settings_section', // Section ID
+        'My Options Title', // Section Title
+        'my_section_options_callback', // Callback
+        'general' // What Page?  This makes the section show up on the General Settings Page
+    );*/
+
+    add_settings_field( // Option 1
+        'option_1', // Option ID
+        'Option 1', // Label
+        'my_textbox_callback', // !important - This is where the args go!
+        'general' // Page it will be displayed (General Settings)
+//        'my_settings_section' // Name of our section
+    );
+    add_settings_field( // Option 1
+        'option_2', // Option ID
+        'Option 2', // Label
+        'my_textbox_callback2', // !important - This is where the args go!
+        'general' // Page it will be displayed (General Settings)
+//        'my_settings_section' // Name of our section
+    );
+    register_setting('general','option_1', 'esc_attr');
+    register_setting('general','option_2', 'esc_attr');
+}
+function my_textbox_callback($args) {  // Textbox Callback
+    $option = get_option('option_1');
+    echo '<input type="text" id="option_1" name="option_1" value="' . $option . '" />';
+}
+function my_textbox_callback2($args) {  // Textbox Callback
+    $option = get_option('option_2');
+    echo '<input type="text" id="option_2" name="option_2" value="' . $option . '" />';
+}
+function my_section_options_callback() { // Section Callback
+    echo '<p>A little message on editing info</p>';
+}
